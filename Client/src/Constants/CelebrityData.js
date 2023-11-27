34a52251b4e68f50e9619035fc983961a52d4cc9
Princegupta101 <pgupta28349@gmail.com>
2023-11-27 16:25:23 +0530

    cleaned up our aboutus page

---
+++ Client/src/Constants/CelebrityData.js
@@ -0,0 +1,40 @@
+ 
+ import apj from '../Assets/Images/apj.png';
+ import billGates from '../Assets/Images/billGates.png'
+ import einstein from '../Assets/Images/einstein.png';
+ import nelsonMandela from '../Assets/Images/nelsonMandela.png';
+ import steveJobs from '../Assets/Images/steveJobs.png';
+
+ export const celebrities=[
+    {
+        title : "Nelson Mandela", 
+        description : "Education is the most powerful weapon which you can use to change the world.",
+        image: nelsonMandela,
+        slideNumber:1,
+    },
+    {
+        title : "A. P. J. Abdul Kalam", 
+        description : "You have to dream before your dreams can come true.",
+        image: apj,
+        slideNumber:2,
+    },
+    {
+        title : "Albert Einstein", 
+        description : "Education is the most powerful tool you can use to change the world .",
+        image: einstein,
+        slideNumber:3,
+    },
+    {
+        title : "Steve Jobs", 
+        description : "Were here to put a dent in the universe. Otherwise why else even be here?",
+        image: steveJobs,
+        slideNumber:4,
+    },
+    {
+        title : "Bill Gates", 
+        description : "Success is a lousy teacher. It seduces smart people into thinking they can't lose.",
+        image: billGates,
+        slideNumber:5,
+    },
+    
+]
